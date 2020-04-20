c89282d30d8e34a5559039c9774cbffca0cd3b48
İhsan <iyazici@ku.edu.tr>
2020-04-20 14:40:12 +0300

    added new stuff

--- designersdream/src/views/Projects.vue
+++ designersdream/src/views/Projects.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class ="projects">
+  <div class ="PROJECTS">
     <h1>Projects</h1>
 
-    <v-container class = "my-5">
-
-      <v-layout row class="mb-3 justify-end">
-
+  <v-container class = "my-5">
+    <v-layout row class="mb-3 justify-end">
         <v-tooltip top>
           <template v-slot:activator="{ on }">
             <v-btn text small color="grey" v-on ="on" @click="sortBy('title')">
@@ -26,36 +24,44 @@
         <span>orders projects by project status</span>
         </v-tooltip>
            
-      </v-layout>
+    </v-layout>
 
-      <v-card outlined hover v-for="project in projects" :key="project.title">
+    <v-expansion-panels outlined hover>
+      <v-expansion-panel v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
-          <v-flex xs12 md6>
-            <div class="caption grey--text">Project title</div>
-            <div>{{project.title}}</div>
-          </v-flex>
+          <v-expansion-panel-header>
+            
+            <v-flex xs12 md6>
+              <div class="caption grey--text">Project title</div>
+              <div>{{project.title}}</div>
+            </v-flex>
 
-          <v-flex xs6 sm4 md2>
-             <div class="caption grey--text">Person</div>
-             <div>{{project.person}}</div>
-          </v-flex>
+            <v-flex xs6 sm4 md2></v-flex>
+            <v-flex xs6 sm4 md2></v-flex>
 
-          <v-flex xs6 sm4 md2>
-             <div class="caption grey--text">Last Changed</div>
-             <div>{{project.lastchanged}}</div>
-          </v-flex>
-          <v-flex xs6 sm4 md2>
-             <div class="text-right">
-               <v-chip small :color="`${project.status}`" :class="`v-chip--active white--text caption my-2`">{{project.status}}</v-chip>
-             </div>
-          </v-flex>
-
+            <v-flex xs6 sm4 md2>
+              <div class="text-right">
+                <v-chip small :color="`${project.status}`" :class="`v-chip--active white--text caption my-2`">{{project.status}}</v-chip>
+              </div>
+            </v-flex>
+          </v-expansion-panel-header>
         </v-layout>
-      </v-card>
+        <v-expansion-panel-content>
+          <v-card>
+            <v-card-text class="px-4 dark--text">
+              <div class="font-weight-bold">Information</div>
+              <div>{{project.content}}</div>
+              <div>
+                <img height= 128 width= 128 v-bind:src="project.src">             
+              </div>            
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
 
-      
 
-        <v-btn small color="primary"  class="my-6 ml-10">
+    <v-btn small color="primary"  class="my-6 ml-10">
           <span class="caption">Select</span>
         </v-btn>
 
@@ -68,9 +74,11 @@
           <v-icon left small>cloud_download</v-icon>
           <span class="caption">Download</span>
         </v-btn>
-      
-    </v-container>
+
+  </v-container>
+    
   </div>
+
 </template>
 
 <script>
@@ -78,12 +86,12 @@ export default {
   data() {
     return {
       projects: [
-        { title: 'My project #1', person: 'İhsan Yazıcı', lastchanged: '28.03.2020', status: 'Done',content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sollicitudin mauris eu nulla accumsan, vitae luctus nisl egestas.'},
-        { title: 'My project #2', person: 'Dursun Bekci', lastchanged: '20.03.2020', status: 'Ongoing',content : 'Aliquam id est diam. Integer viverra placerat porta. Nam eget pellentesque libero. Morbi vestibulum sem iaculis sagittis pulvinar. Donec'},
-        { title: 'My project #3', person: 'Ayça Avcı'   , lastchanged: '22.03.2020', status: 'Done',content : 'diam mauris, tempus vel finibus sed, consectetur et tellus. Vivamus laoreet justo vitae dui tincidunt, at placerat nibh bibendum. Vivamus'},
-        { title: 'My project #4', person: 'İhsan Yazıcı', lastchanged: '21.02.2020', status: 'Done',content : 'fringilla diam sed mi semper bibendum. Nulla justo sapien, sollicitudin vitae hendrerit bibendum, tempus nec ex. Aliquam felis quam,'},
-        { title: 'My project #5', person: 'Genco Levi'  , lastchanged: '12.01.2020', status: 'Ongoing',content : 'convallis sed leo sit amet, auctor pharetra ligula. Vestibulum in augue ac ex tristique blandit vitae nec enim. Ut aliquet risus vitae erat'},
-        { title: 'My project #6', person: 'Genco Levi'  , lastchanged: '23.03.2020', status: 'Done',content : 'convallis ornare. Vestibulum eget orci sed quam laoreet fermentum vel sollicitudin arcu. Pellentesque sed metus sem. Vivamus libero nisl, scelerisque sed ultrices eu, posuere sit amet leo.'}
+        { title: 'My project #1', status: 'Done'   ,src:'/handbag.png' , content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sollicitudin mauris eu nulla accumsan, vitae luctus nisl egestas.'},
+        { title: 'My project #2', status: 'Ongoing',src:'/handbag1.png', content : 'Aliquam id est diam. Integer viverra placerat porta. Nam eget pellentesque libero. Morbi vestibulum sem iaculis sagittis pulvinar. Donec'},
+        { title: 'My project #3', status: 'Done'   ,src:'/handbag2.png', content : 'diam mauris, tempus vel finibus sed, consectetur et tellus. Vivamus laoreet justo vitae dui tincidunt, at placerat nibh bibendum. Vivamus'},
+        { title: 'My project #4', status: 'Done'   ,src:'/handbag3.png', content : 'fringilla diam sed mi semper bibendum. Nulla justo sapien, sollicitudin vitae hendrerit bibendum, tempus nec ex. Aliquam felis quam,'},
+        { title: 'My project #5', status: 'Ongoing',src:'/handbag1.png', content : 'convallis sed leo sit amet, auctor pharetra ligula. Vestibulum in augue ac ex tristique blandit vitae nec enim. Ut aliquet risus vitae erat'},
+        { title: 'My project #6', status: 'Done'   ,src:'/handbag2.png', content : 'convallis ornare. Vestibulum eget orci sed quam laoreet fermentum vel sollicitudin arcu. Pellentesque sed metus sem. Vivamus libero nisl, scelerisque sed ultrices eu, posuere sit amet leo.'}
       ]
     }
   },
